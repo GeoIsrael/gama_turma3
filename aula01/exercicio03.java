b1f7adb1e5052149815c981a2889ef77f92b139c
GeoIsrael <israelsallesnogueira53@gmail.com>
2022-03-16 13:47:33 -0300

    exercicio 03

---
+++ aula01/exercicio03.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class exercicio03 {
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+        int numero, contador;
+
+        contador=1;
+
+
+        numero= teclado.nextInt();
+
+
+        while (contador<=numero){
+            System.out.println(contador);
+            contador++;
+
+        }
+
+    }
+}
