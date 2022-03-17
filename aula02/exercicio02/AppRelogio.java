fac3846b64afc98c10ff526047fe9eccced5e579
GeoIsrael <israelsallesnogueira53@gmail.com>
2022-03-17 14:06:40 -0300

    praticando o main

--- aula02/exercicio02/AppRelogio.java
+++ aula02/exercicio02/AppRelogio.java
@@ -2,6 +2,8 @@ package exercicio02;
 
 public class AppRelogio {
     public static void main(String[] args) {
-        
+        Relogio relogio1 = new Relogio(23, 48, 6);
+
+        System.out.println(relogio1.getHoraAtual());
     }
 }
