8a16b68429800be13a4ca6a67b5a2ef54e61a421
GeoIsrael <israelsallesnogueira53@gmail.com>
2022-03-17 14:06:17 -0300

    Atualizações no exercicio 3 da aula 02

--- aula02/exercicio03/AppLivro.java
+++ aula02/exercicio03/AppLivro.java
@@ -2,17 +2,11 @@ package exercicio03;
 
 public class AppLivro {
     public static void main(String[] args) {
-        Ebook livro1 = new Ebook("Machado de Assis", "autor", 10);
+        Ebook livro1 = new Ebook("Machado de Assis", "autor", -1);
 
         livro1.mostrar();
 
         livro1.avancarUmaPagina();
-
-        livro1.mostrar();
-
-        livro1.voltarUmaPagina();
-
-        livro1.mudarPaginaAtual(30);
 
         livro1.mostrar();
     }
--- aula02/exercicio03/Ebook.java
+++ aula02/exercicio03/Ebook.java
@@ -7,20 +7,25 @@ public class Ebook {
     public Ebook (String titulo, String autor,int totalDePaginas){
         this.titulo=titulo;
         this.autor = autor;
-        this.totalDePaginas=totalDePaginas;
+        setTotalDePaginas(totalDePaginas);
         this.NumeroDaPaginaAtual=1;
     }
 
     public void avancarUmaPagina(){
+        if(NumeroDaPaginaAtual<totalDePaginas)
         NumeroDaPaginaAtual++;
     }
 
     public void voltarUmaPagina(){
+        if(NumeroDaPaginaAtual>1)
         NumeroDaPaginaAtual--;
     }
 
     public int mudarPaginaAtual(int novaPagina){
+        if (novaPagina<=this.totalDePaginas)
         this.NumeroDaPaginaAtual=novaPagina;
+        else
+        System.out.println("O livro não tem essa pagina");
         return NumeroDaPaginaAtual;
     }
 
@@ -29,10 +34,23 @@ public class Ebook {
     }
 
     public void mostrar(){
-        System.out.println(titulo);
-        System.out.println(autor);
-        System.out.println(totalDePaginas);
-        System.out.println(NumeroDaPaginaAtual);
+        System.out.println("*****__ Bem-Vindo ao APP de Livros Online *-* __****");
+        System.out.println("O Titulo do seu Atual livro é: "+titulo);
+        System.out.println("O autor do seu Livro é: "+autor);
+        System.out.println("Você está na pagina: "+NumeroDaPaginaAtual);
+        System.out.println("O seu livro tem: "+totalDePaginas+" Paginas");
+        System.out.println("");
     }
+
+    public int getTotalDePaginas() {
+        return totalDePaginas;
+    }
+
+    public void setTotalDePaginas(int totalDePaginas) {
+        if (totalDePaginas>0)
+        this.totalDePaginas = totalDePaginas;
+    }
+
+    
 
 }
