27dd522f4d026132e9367421be188189aee0993d
GeoIsrael <israelsallesnogueira53@gmail.com>
2022-03-18 12:00:36 -0300

    Exercicio 02 da aula 03

---
+++ aula03/exercicio02/AppPlacar.java
@@ -0,0 +1,15 @@
+public class AppPlacar {
+    public static void main(String[] args) {
+
+        Placar jogo1 = new Placar("Fortaleza", "Ceara", 10, -2);
+        Placar jogo2 = new Placar("Fortaleza", "Flamengo");
+        Placar jogo3 = new Placar();
+
+        jogo1.mostrarPlacar();
+        jogo2.mostrarPlacar();
+        jogo3.mostrarPlacar();
+        
+
+
+    }
+}
