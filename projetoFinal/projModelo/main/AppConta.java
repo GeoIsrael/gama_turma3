2bc2b6fe1f3d8bdc93936e6d464ec3efbf505a7f
GeoIsrael <israelsallesnogueira53@gmail.com>
2022-03-23 10:33:56 -0300

    adicionando opções de saque e deposito

--- projetoFinal/projModelo/main/AppConta.java
+++ projetoFinal/projModelo/main/AppConta.java
@@ -14,16 +14,20 @@ public class AppConta {
         int opcao = 0;
         int numero;
         double saldo;
+        double valorASacar;
         String entradaDigitada;
         Conta c;
 
-        while (opcao != 4) {
+        while (opcao != 7) {
             System.out.println("****** BEM-VINDO AO NEWBANK S2******");
             System.out.println("Oque Desejas?");
-            System.out.println("1-Criar Conta Corrente");
-            System.out.println("2-Criar Conta Especial");
-            System.out.println("3-Consultar Conta");
-            System.out.println("4-Sair");
+            System.out.println("1 - Criar Conta Corrente");
+            System.out.println("2 - Criar Conta Especial");
+            System.out.println("3 - Consultar Conta");
+            System.out.println("4 - Efetuar Saque em uma conta");
+            System.out.println("5 - Efetuar Deposito em uma conta");
+            System.out.println("6 - Excluir Conta");
+            System.out.println("7 - Sair");
 
             entradaDigitada = teclado.nextLine();
             opcao = Integer.parseInt(entradaDigitada);
@@ -59,6 +63,26 @@ public class AppConta {
                     break;
 
                 case 4:
+                    System.out.println("Digite o numero da conta:");
+                    entradaDigitada = teclado.nextLine();
+                    numero = Integer.parseInt(entradaDigitada);
+                    System.out.println("Digite o valor  para sacar:");
+                    entradaDigitada = teclado.nextLine();
+                    valorASacar = Integer.parseInt(entradaDigitada);
+
+                case 5:
+                    System.out.println("Digite o código a ser removido:");
+                    entradaDigitada = teclado.nextLine();
+                    numero = Integer.parseInt(entradaDigitada);
+                    boolean removeu = contas.remover(numero);
+                    if(removeu) {
+                        System.out.println("Removido com sucesso");
+                    } else {
+                        System.out.println("Não foi possível remover");
+                    }
+                    break;
+
+                case 6:
                     System.out.println("Fim do programa.");
                     break;
 
